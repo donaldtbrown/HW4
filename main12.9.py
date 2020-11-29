2ed59066318a4cc88bce10ae6949a9c44b0bc77f
Donald Brown <70461504+donaldtbrown@users.noreply.github.com>
2020-11-28 18:13:44 -0600

    Add files via upload

---
+++ main12.9.py
@@ -0,0 +1,14 @@
+#Donald Brown
+#1943301
+# Split input into 2 parts: name and age
+parts = input().split()
+name = parts[0]
+while name != '-1':
+    try:
+        age = int(parts[1]) + 1
+    except Exception as ex:
+        age = 0
+    # Get next line
+    print('{} {}'.format(name, age))
+    parts = input().split()
+    name = parts[0]
